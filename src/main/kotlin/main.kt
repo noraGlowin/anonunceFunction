e5fb2b7e85b5c7d0a7adcb620266274301322449
Nora <Norah.Nahedh.ruh.k@tuwaiq.edu.sa>
2021-10-18 01:11:42 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,47 @@
+fun main(){
+    var Queen= "Nora" //.count({letter -> letter=='o'})
+    println(Queen)
+    println(
+        {
+        var currentYear =2021
+        "Welcome $Queen your journey with kotlin started ($currentYear)"
+}()
+    )
+// %%%%%%%%%%%%%%%%%%%%5.2 Assigning the anonymous function to a variable %%%%%%%%%%%%%%%%%%%%
+//    val sayHelloZoro : ()-> String ={
+//        val favoriteAnime= "Zoro"
+//        "$favoriteAnime is  $Queen's favorite animetion in 2000 "
+//
+//    }
+//println(sayHelloZoro())
+//    %%%%%%%%%%%%%%%%%%%%5.3  Adding a parameter to the anonymous function %%%%%%%%%%%%%%%%%%%%
+//    val sayHelloZoro : (String)-> String ={favoriteAnime ->
+//
+//        "$favoriteAnime is  $Queen's favorite animetion in 2000 "
+//
+//    }
+//    println(sayHelloZoro("Zoro"))
+ /// %%%%%%%%%%%%%%%%%%%%5.4  Using the it keyword %%%%%%%%%%%%%%%%%%%%
+//    val sayHelloZoro : (String) -> String={it
+//        "$it is  $Queen's favorite animetion in 2000 "
+//
+//    }
+//    println(sayHelloZoro("Zoro"))
+    //%%%%%%%%%%%%%%%%%%%%5.5  Accepting a second argument%%%%%%%%%%%%%%%%%%%%
+//    val sayHelloZoro :(String , Int) -> String ={favoriteAnime , releaseDate ->
+//        "$favoriteAnime is  $Queen's favorite animetion in 2000 it was  released in $releaseDate"
+//    }
+//    println(sayHelloZoro("Zoro", 1996))
+    //%%%%%%%%%%%%%%%%%%%%5.6  Using type inference%%%%%%%%%%%%%%%%%%%%
+    val sayHelloZoro ={favoriteAnime :String,releaseDate:Int ->
+        "$favoriteAnime is  $Queen's favorite animetion in 2000 it was  released in $releaseDate"
+
+    }
+    println(sayHelloZoro("Zoro", 1996))
+
+    println(favoriteStuff("kpop",sayHelloZoro))
+}
+fun favoriteStuff (music:String ,sayHelloZoro:(String,Int)->String):String{
+    var zoro = sayHelloZoro("Zoro", 1996)
+return "$music is Nora's favorite music and $zoro"
+}
